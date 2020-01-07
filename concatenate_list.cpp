d836b88e20bed9b1f51e868dd4a3c75c117a3b00
jaeyoon-lee2 <54957190+jaeyoon-lee2@users.noreply.github.com>
2020-01-06 23:54:19 -0500

    commit message

--- concatenate_list.cpp
+++ concatenate_list.cpp
@@ -10,12 +10,45 @@
 #include <sstream>
 
 
+template<size_t N>
+std::array<std::string, 6>* ConcatenateList(std::string firstThings,
+                            std::string secondThings) {
+    std::array<std::string, 3> firstList;
+    std::array<std::string, 3> secondList;
+    std::array<std::string*, 6> concatenatedList;
+
+    std::stringstream firstSplit(firstThings);
+    std::string token1;
+    int counter1 = 0;
+
+    while (getline(firstSplit, token1, '_')) {
+      firstList[counter1] = token1;
+      counter1 += 1;
+    }
+    std::stringstream secondSplit(secondThings);
+    std::string token2;
+    int counter2 = 0;
+
+    while (getline(secondSplit, token2, '_')) {
+      secondList[counter2] = token2;
+      counter2 += 1;
+    }
+
+    for (int counter = 0; counter < 6; counter++) {
+        if (counter < 3) {
+            concatenatedList[counter] = firstList[counter];
+        } else {
+            concatenatedList[counter] = secondList[counter-3];
+        }
+    }
+    return concatenatedList;
+}
+
+
 main() {
     // this function concatenate two lists
 
-    std::array<std::string, 3> firstList;
-    std::array<std::string, 3> secondList;
-    std::array<std::string, 6> concatenatedList;
+    std::array<std::string*, 6> concatenatedList;
     std::string firstThings;
     std::string secondThings;
     std::string firstListName;
@@ -33,34 +66,12 @@ main() {
     std::cout << "Enter three things to put in "<< firstListName
               <<" (Separated by '_'): ";
     std::cin >> firstThings;
-    std::stringstream firstSplit(firstThings);
-    std::string token1;
-    int counter1 = 0;
-
-    while (getline(firstSplit, token1, '_')) {
-      firstList[counter1] = token1;
-      counter1 += 1;
-    }
 
     std::cout << "Enter three things to put in "<< secondListName
               <<" (Separated by '_'): ";
     std::cin >> secondThings;
-    std::stringstream secondSplit(secondThings);
-    std::string token2;
-    int counter2 = 0;
 
-    while (getline(secondSplit, token2, '_')) {
-      secondList[counter2] = token2;
-      counter2 += 1;
-    }
-
-    for (int counter = 0; counter < 6; counter++) {
-        if (counter < 3) {
-            concatenatedList[counter] = firstList[counter];
-        } else {
-            concatenatedList[counter] = secondList[counter-3];
-        }
-    }
+    concatenatedList = ConcatenateList(firstThings, secondThings);
 
     // output
     std::cout << "" << std::endl;
